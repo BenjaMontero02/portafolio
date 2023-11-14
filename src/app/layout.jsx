74cd4f677f921265acdec6ed95535a78c8eb9a7b
cacha <monterobenja.2018@gmail.com>
2023-11-14 01:01:47 -0300

    menuheader

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -7,7 +7,7 @@ export default function RootLayout({ children }) {
       <head>
         <title>Benjamin Montero</title>        
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css"></link>
-
+        <link rel="stylesheet" href="./globals.css" />
       </head>
       <body>
         <Header/>
